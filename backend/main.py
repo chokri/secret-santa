1e5e9f1407ace79e3356be882477080d34c85d19
Chokri K <chokri@khalifa.tn>
2025-01-20 10:15:19 +0100

    Add backend FastAPI with a healthcheck route

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from modules.users.routes import router as users_routes
+from starlette.datastructures import CommaSeparatedStrings
+
+stage = os.environ.get('STAGE', None)
+ALLOWED_HOSTS=CommaSeparatedStrings(os.environ.get('ALLOWED_HOSTS', ''))
+
+app = FastAPI(
+    title="Secret Santa API",
+    description="Backend API fro Secret Santa Application",
+    root_path=f"/{stage}" if stage else "/",
+    version="0.0.1"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_HOSTS or ["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(users_routes, prefix="/api")
